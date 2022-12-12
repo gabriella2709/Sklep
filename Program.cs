16b5545bca9a0df2e3425a38bcb78a278942131d
Z1NG0D <115076622+Z1NG0D@users.noreply.github.com>
2022-12-12 13:26:04 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,10 @@
-﻿
 using Sklep;
-
 
 var kasa = new Kasa();
 kasa.CreateProduct();
 var menu = new Menu();
-
-// The 'Wybrane' class is completely unnecessary.
-// After all, the shopping cart (paragon) contains the same products as in the store.
-// One 'Product' class is enough!!!
+var now = DateTime.Now;
+var AllProducts = kasa.AllProducts!;
 var wybrane = new List<Product>();
 var products = kasa.AllProducts;
 menu.Start();
@@ -31,53 +27,77 @@ while (true)
     {
         Console.Clear();
         menu.Zakup();
-        if (menu.answer == "P")
-        {
-
-                // We must have two values: cena_laczna (this is a sum of all values of cena_brutto of products in paragon)
-                // and total_vat (this is a sum of all values of VAT of products in paragon)
-                // So we have to options to choose:
-                // 1 option (using loop)
-                decimal cena_laczna = 0;
-                decimal total_vat = 0;
-                foreach (var p in wybrane)
-                {
-                    cena_laczna += p.Cena_brutto;
-                    total_vat += p.VAT;
-                }
-                
-                // 2 option (using 'Sum' method)
-                cena_laczna = wybrane.Sum(w => w.Cena_brutto);
-                total_vat = wybrane.Sum(w => w.VAT);
-                
-                // Please choose one of those options (whatever you want)
-
-        }
-        else if (menu.answer == "p")
-        {
-            System.Console.WriteLine(wybrane);
-            break;
-        }
-
     }
+    if (!menu.IsCorrect(menu.answer!) && menu.answer == "3")
+    {
+        break;
+    }
+    if (menu.answer == "P")
+    {
+        Console.Clear();
+        Console.WriteLine("___________________________________________________");
+        Console.WriteLine("PARAGON");
+        System.Console.WriteLine("DATA ZAKUPU: " + now.ToString("F"));
+        Console.WriteLine("___________________________________________________");
 
-    var AllProducts = kasa.AllProducts!;
+        foreach (var p in wybrane)
+        {
+            System.Console.WriteLine(p.Nazwa + "||" + p.Cena_netto + " zł");
+        }
+        decimal cena_laczna = 0;
+        decimal total_vat = 0;
+        foreach (var p in wybrane)
+        {
+            cena_laczna += p.Cena_brutto;
+            total_vat += p.VAT;
+        }
+        System.Console.WriteLine("___________________________________________________");
+        System.Console.WriteLine("DO ZAPŁATY: " + cena_laczna + " zł");
+        System.Console.WriteLine("W TYM VAT: " + total_vat + " zł");
+        System.Console.WriteLine("___________________________________________________");
+        break;
+    }
+    else if (menu.answer == "p")
+    {
+        Console.Clear();
+        Console.WriteLine("___________________________________________________");
+        Console.WriteLine("PARAGON");
+        System.Console.WriteLine("DATA ZAKUPU: " + now.ToString("F"));
+        Console.WriteLine("___________________________________________________");
 
+        foreach (var p in wybrane)
+        {
+            System.Console.WriteLine(p.Nazwa + "||" + p.Cena_netto + " zł");
+        }
+        decimal cena_laczna = 0;
+        decimal total_vat = 0;
+        foreach (var p in wybrane)
+        {
+            cena_laczna += p.Cena_brutto;
+            total_vat += p.VAT;
+        }
+        System.Console.WriteLine("___________________________________________________");
+        System.Console.WriteLine("DO ZAPŁATY: " + cena_laczna + " zł");
+        System.Console.WriteLine("W TYM VAT: " + total_vat + " zł");
+        System.Console.WriteLine("___________________________________________________");
+        break;
+    }
     foreach (var p in AllProducts)
     {
         if (menu.answer == p.Id)
         {
-
-            // new Wybrane().Nazwa = p.Nazwa;
-            // new Wybrane().Podatek = p.Cena_netto * 0.23m;
-            // new Wybrane().Cena_brutto = Math.Round(p.Cena_netto + new Wybrane().Podatek, 2);
-            // Console.WriteLine(p.Nazwa);
-            // Console.WriteLine("CENA ŁĄCZNA: " + new Wybrane().Cena_brutto + " zł");
-            // wybrane.Add(new Wybrane());
-            // menu.Zakup();
-
-
+            Console.Clear();
+            var p1 = p;
+            System.Console.WriteLine(p.Nazwa);
+            Console.WriteLine("CENA ŁĄCZNA: " + Math.Round(p.Cena_brutto + p.VAT, 2) + " zł");
+            menu.Zakup();
         }
     }
 }
+
+
+
+
+
+
 
